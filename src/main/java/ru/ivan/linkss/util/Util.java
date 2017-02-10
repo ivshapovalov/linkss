16bc1f9428b0b6323a4709f016504ed82311b0cf
Ivan <ivshapovalov@gmail.com>
2017-02-10 15:36:52 +0300

    add FTP manager

--- src/main/java/ru/ivan/linkss/util/Util.java
+++ src/main/java/ru/ivan/linkss/util/Util.java
@@ -1,10 +1,5 @@
 package ru.ivan.linkss.util;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.net.URI;
@@ -30,27 +25,7 @@ public class Util {
             return "";
         }
     }
-    public static void downloadImageFromS3(String filePath, String key) {
 
-        AmazonS3 s3 = new AmazonS3Client();
-        try {
-            String bucketName=System.getenv("S3_BUCKET_NAME");
-            S3Object o = s3.getObject(bucketName, key);
-            S3ObjectInputStream s3is = o.getObjectContent();
-            File file = new File(filePath);
-
-            FileOutputStream fos = new FileOutputStream(file);
-            byte[] read_buf = new byte[1024];
-            int read_len = 0;
-            while ((read_len = s3is.read(read_buf)) > 0) {
-                fos.write(read_buf, 0, read_len);
-            }
-            s3is.close();
-            fos.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
     public static void downloadImageFromFTP(String filePath, String key) {
 
         FTPManager ftpManager =
